060d7b195852ae99ae35852ab94917913def9ea4
Raphael <reuphy@gmail.com>
2019-09-27 22:31:54 +0200

    testing directive in angular

--- pokemonsapp/src/app/app.module.ts
+++ pokemonsapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './directives/border-card.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderCardDirective
   ],
   imports: [
     BrowserModule,
---
+++ pokemonsapp/src/app/directives/border-card.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
+//angular create a new instance of the directive every time he detects an element html that has the attribute pkmnBorderCard
+//and inject the dom element in the constructor and functions to modify it.
+  constructor(private el: ElementRef) {
+		this.setBorder('#f5f5f5');
+		this.setHeight(180);
+	}
+
+	private setBorder(color: string) {
+		let border = 'solid 4px ' + color;
+		this.el.nativeElement.style.border = border;
+	}
+
+	private setHeight(height: number) {
+		this.el.nativeElement.style.height = height + 'px';
+	}
+}
