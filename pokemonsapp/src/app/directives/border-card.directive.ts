d4623f545d2414c8cf30c97c746e56b8b9227262
Raphael <reuphy@gmail.com>
2019-09-28 09:50:15 +0200

    //refactoring of the code the goal is to delete the hardcoded value and replace them with declared variables.

--- pokemonsapp/src/app/directives/border-card.directive.ts
+++ pokemonsapp/src/app/directives/border-card.directive.ts
@@ -4,22 +4,28 @@ import { Directive, ElementRef, HostListener,Input } from '@angular/core';
   selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
+
+  //refactoring of the code the goal is to delete the hardcoded value and replace them with declared variables.
+  private initialColor:string = "#f5f5f5";
+  private defaultColor:string = "#009688";
+  private defaultHeight:number = 180;
+
 //angular create a new instance of the directive every time he detects an element html that has the attribute pkmnBorderCard
 //and inject the dom element in the constructor and functions to modify it.
   constructor(private el: ElementRef) {
-		this.setBorder('#f5f5f5');
-		this.setHeight(180);
+		this.setBorder(this.initialColor);
+		this.setHeight(this.defaultHeight);
   }
-
+  
   @Input('pkmnBorderCard') borderColor:string;
   
   @HostListener('mouseenter') onMouseEnter(){
-		this.setBorder(this.borderColor || '#009688');
+		this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-		this.setBorder('#f5f5f5');
-  } 
+	this.setBorder(this.initialColor);
+} 
 
 	private setBorder(color: string) {
 		let border = 'solid 4px ' + color;
